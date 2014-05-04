c38976ba1f4be0beccacf4144481b5e18a37bf52
LesyukSerg <seranatles@gmail.com>
2014-05-04 11:05:49 +0300

    20130807
    додано виключення файлу самого архіватора із архіву
    додана функція flush, завдяки якій повинен точніше відображатись хід
    архівації

--- archiver.php
+++ archiver.php
@@ -1,4 +1,25 @@
-<? set_time_limit(0); ?>
+<?/*
+	-archiver_201307311120
+		метод выдправки форми змінений на POST
+		змінена перевірка на розмір файлів
+
+	-archiver_201308021520
+		додана можливість обчислення кількості файлів
+		вибрані налаштування зберігаються після створення архіву
+		трохи змінений інтерфейс
+
+	-archiver_201308060806_edit_by_artem
+		виправлені помилки при роботы в браузері Opera
+
+	-archiver_201308061321
+		додана можливість розархівації
+
+	-archiver_201308071023
+		додано виключення файлу самого архыватора із архіву
+		додана функція flush, завдяки якій повинен точніше відображатись хід архівації
+*/
+	set_time_limit(0);
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -13,10 +34,10 @@
 			*/
 		</style>
 	</head>
-<?	
-	# папка в которой будет размещен архив
+<?
+	# тека в якій буде размішено архів
 	$archive_dir = dirname(__FILE__);
-	
+
 	$dirs = scandir($archive_dir);
 	$count = 0;
 	$archive_dir = $archive_dir."/";
@@ -27,11 +48,10 @@
 	} else {
 		$all_count = " багацько";
 	}
-	# Подсчет всех файлов в корне -------------
+	# Підрахунок всіх файлів в корені -------------
 	function addFolderCount($dir, &$cnt){
 		if ($dh = opendir($dir)) {
-			#  цикл по всем файлам
-			while (($file = readdir($dh)) !== false) {				
+			while (($file = readdir($dh)) !== false) {
 				if(is_dir($dir . $file)){
 					if($file !== "." && $file !== ".."){
 						addFolderCount($dir . $file . "/", $cnt);
@@ -42,35 +62,44 @@
 			}
 		}
 	}
-	
-	# Рекурсивная функция просмотра и архивации вложеных файлов и папок
+
+	# Рекурсивна функція архівації вкладених файлів і тек
 	function addFolderToZip($dir, &$zipArchive, $zipdir = '', &$cnt){
 		if (is_dir($dir)) {
 			echo "<b><span title='".$dir."'> * </span></b>";
+			flush();
+
 			if ($dh = opendir($dir)) {
-				# Добавляем пустую директорию
-				if(!empty($zipdir)) $zipArchive->addEmptyDir($zipdir);
-			  
-				#  цикл по всем файлам
+				# Додаємо порожню директорію
+				if(!empty($zipdir)){
+					if($zipArchive->addEmptyDir($zipdir) === false){
+						echo "Помилка при додаванні порожної теки - $zipdir";
+					}
+				}
+			
+				#  цикл по всім файлам
 				while (($file = readdir($dh)) !== false) {
-			  
-					# если это папка запускаем функцию опять
+					# якщо це тека - запускаємо функцію
 					if(is_dir($dir . $file)){
-						# пропуск директорий '.' и '..'
+						# пропуск директорій '.' і '..'
 						if( ($file !== ".") && $file !== ".." && !in_array($file, explode("|",$_POST['exept'])) ){
 							addFolderToZip($dir . $file . "/", $zipArchive, $zipdir . $file . "/", $cnt);
 						}
 					}else{
-						# Добавляем файлы в архив
+						# Додаємо файли в архів
 						if($file !==__FILE__){
 							$cnt++;
 							if($cnt > $_POST['max']){
 								break;
 							}
-							
-							if($cnt > $_POST['min'] && filesize($dir.$file) < $_POST['max_size']*1024 ){
-								$zipArchive->addFile($dir . $file, $zipdir . $file);
-								echo "<span title='".$dir.$file."'> . </span>";
+
+							if($cnt > $_POST['min'] && filesize($dir.$file) < $_POST['max_size']*1024 && $file != basename(__FILE__) && !strstr($file, "zip")){
+								if($zipArchive->addFile($dir . $file, $zipdir . $file)){
+									echo "<span title='".$dir.$file."'> . </span>";
+								} else {
+									echo "Помилка архівації: файл $dir.$file";
+								}
+								flush();
 								// echo (1000000+$cnt)." - ".$dir.$file." OK <br />";
 							}
 						}
@@ -84,62 +113,62 @@
 		<div id="wpapper">
 			<h1>Архіватор сайту</h1>
 			<div id="form_block">
-				<form enctype="multipart/form-data" action="<?=$_SERVER['REQUEST_URI']?>" method="POST">
-					<fieldset class="right">
-						<legend title="">Кількість файлів:</legend>
-						Всього файлів <b><?=$all_count?></b>
-						<input type="checkbox" id="get_count" name="get_count" value='1' checked="checked" onclick="if(get_count.checked)window.location=window.location.href+'/?get_count=1'; else window.location='<?='http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']?>'" /> Показати кількість файлів.(Це займе деякий час...)<br />
-					</fieldset>
-					<fieldset>
-						<legend title="" >Знайдені архіви:</legend>
+				<fieldset class="right">
+					<legend title="">Кількість файлів:</legend>
+					Всього файлів <b><?=$all_count?></b>
+					<input type="checkbox" id="get_count" name="get_count" value='1' checked="checked" onclick="if(get_count.checked)window.location=window.location.href+'/?get_count=1'; else window.location='<?='http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']?>'" /> Показати кількість файлів.(Це займе деякий час...)<br />
+				</fieldset>
+				<fieldset>
+					<legend title="" >Знайдені архіви:</legend>
 <?
-							foreach($dirs as $dir){
-								if (!is_dir($archive_dir.$dir) && $dir != "." && $dir != ".." && strstr($dir, "zip")){
+						foreach($dirs as $dir){
+							if (!is_dir($archive_dir.$dir) && $dir != "." && $dir != ".." && strstr($dir, "zip")){
 ?>
-									<form enctype="multipart/form-data" action="<?=$_SERVER['REQUEST_URI']?>" method="POST">
-										<input class="selectedzip" type="radio" name="zipfile" value="<?=$dir?>" checked="checked" /> <?=$dir?> <input type="submit" name="unzip" value="Видобути архів" />
-									</form>
+								<form enctype="multipart/form-data" action="<?=$_SERVER['REQUEST_URI']?>" method="POST">
+									<input class="selectedzip" type="radio" name="zipfile" value="<?=$dir?>" checked="checked" /> <?=$dir?> <input type="submit" name="unzip" value="Видобути архів" />
+								</form>
 <?
-								}
 							}
+						}
 ?>
-					</fieldset>
-					<fieldset>
-						<legend title="" >Хід виконання розархівації:</legend>
-						<div>
+				</fieldset>
+				<fieldset>
+					<legend title="" >Хід виконання розархівації:</legend>
+					<div>
 <?
-							if($_POST['unzip']){
-								if($_POST['zipfile']) {
-									$zipfile = $archive_dir.$_POST['zipfile'];
-									
-									if(file_exists($zipfile)){
-										$zip = new ZipArchive;
-										$res = $zip->open($zipfile);
-										if ($res === TRUE) {
-											$zip->extractTo($archive_dir);
-											$zip->close();
-											echo "Ура! Архів <b>".$_POST['zipfile']."</b> успішно відобуто в директорію $archive_dir";
-										} else {
-											echo "Архів <b>".$_POST['zipfile']."</b> пожкоджено";
-										}
+						if($_POST['unzip']){
+							if($_POST['zipfile']) {
+								$zipfile = $archive_dir.$_POST['zipfile'];
+
+								if(file_exists($zipfile)){
+									$zip = new ZipArchive;
+									$res = $zip->open($zipfile);
+									if ($res === TRUE) {
+										$zip->extractTo($archive_dir);
+										$zip->close();
+										echo "Ура! Архів <b>".$_POST['zipfile']."</b> успішно відобуто в директорію $archive_dir";
 									} else {
-										echo "Файл не знайдено";
+										echo "Архів <b>".$_POST['zipfile']."</b> пожкоджено";
 									}
 								} else {
-									echo "Виберіть файл для розархівування";
+									echo "Файл не знайдено";
 								}
+							} else {
+								echo "Виберіть файл для розархівування";
 							}
-	?>
-						</div>
-					</fieldset>
-					<hr /><hr /><hr />
+						}
+?>
+					</div>
+				</fieldset>
+				<hr /><hr /><hr />
+				<form action="<?=$_SERVER['REQUEST_URI']?>" method="POST">
 					<fieldset class="left">
 						<legend title="">Виберіть директорію для архівування:</legend>
 						<script>
 							<?if(!$_GET['get_count']):?>
 								document.getElementById('get_count').checked = false;
 							<?endif;?>
-							
+
 							function turn_of(alldir){
 								if(alldir.checked) {
 									var f1 = document.getElementsByTagName('input');
@@ -164,12 +193,12 @@
 							<input type="text" name="dir_write" value="<?=$_POST['dir_write']?>" style="width:99%" /> <br />
 						</fieldset>
 					</fieldset>
-				
+
 					<fieldset class="right">
 						<legend title="" >Які теки слід виключити: вводити через "|"</legend>
 						<textarea cols="10" rows="4" name="exept" style="resize:none; width: 99%"><?=$_POST['exept']?></textarea>
 					</fieldset>
-				
+
 					<fieldset class="right">
 						<legend title="" >Не архівувати файли більше ніж:</legend>
 <?
@@ -177,7 +206,7 @@
 ?>
 						<input type="text" name="max_size" value="<?=$_POST['max_size']?>"  /> кб
 					</fieldset>
-					
+
 					<fieldset class="right">
 						<legend title="" >Обмеження за кількістю файлів:</legend>
 <?
@@ -199,11 +228,11 @@
 								unset($_POST['dir']);
 								$_POST['dir'] = $_POST['dir_write'];
 							}
-							
+
 							if(isset($_POST['dir'])){
-								# создание zip архива
+								# створення zip архіву
 								$zip = new ZipArchive();
-								# имя файла архива
+								# ім'я архіва
 								if(is_array($_POST['dir'])) {
 									$archname = implode("-",$_POST['dir'])."-backup_".date('Y_m_d_H_i_s').".zip";
 								} elseif($_POST['dir_write']) {
@@ -218,10 +247,9 @@
 									exit(1);
 								}
 
-								# добавляем файлы в архив все файлы из папки src_dir								
+								# додаємо файли в архів всі файли із теки src_dir
 								if(is_array($_POST['dir'])) {
 									foreach($_POST['dir'] as $onedir){
-										# папка с исходными файлами
 										$src_dir = dirname(__FILE__)."/".$onedir."/";
 										addFolderToZip($src_dir, $zip, $onedir."/", $count);
 									}
@@ -229,8 +257,8 @@
 									$src_dir = dirname(__FILE__)."/".$_POST['dir']."/";
 									addFolderToZip($src_dir, $zip, '', $count);
 								}
-								
-								# закрываем архив
+
+								# закриваемо архів
 								$zip->close();
 
 								echo "<br />Архів <a href='$archname'>".$archname."</a> створено. Він вміщує ".$count." файлів";
