39855ded5ece7ff41cfa027c802d8304e4cd5952
Lesyuk Serg <seranatles@gmail.com>
2015-12-15 10:40:55 +0200

    format checker

--- checker.php
+++ checker.php
@@ -1,27 +1,1 @@
-<?
-	if(!$_GET['dwnld']){
-		$pathname = getcwd();
-		$dirs = scandir($pathname);
-		
-		$check = "/\d+/";
-		$last = 0;
-		foreach($dirs as $file){
-			preg_match($check, $file, $vers);
-			
-			if($vers[0] > $last){
-				$last = $vers[0];
-			}
-		}
-
-		if($_GET['curr'] < $last){
-			echo "Остання версія <a title='Завантажити' href='http://lesyuk-serg.zz.mu/archiver/checker.php?dwnld=".$last."'>".$last."</a>";
-		} else {
-			echo $last;
-		}
-	} else {
-		header('Content-type: application/php');
-		$file = "archiver_".$_GET['dwnld'].".php";
-		header('Content-Disposition: attachment; filename="'.$file.'"');
-		readfile($file);
-	}
-?>
+<?    if (!$_GET['dwnld']) {        $pathname = getcwd();        $dirs = scandir($pathname);        $check = "/\d+/";        $last = 0;        foreach ($dirs as $file) {            preg_match($check, $file, $version);            if ($version[0] > $last) {                $last = $version[0];            }        }        if ($_GET['curr'] < $last) {            echo "<a title='Завантажити' href='http://{$_SERVER['SERVER_NAME']}/archiver/checker.php?dwnld={$last}'>{$last}</a>";        } else {            echo $last;        }    } else {        header('Content-type: application/php');        $file = "archiver_{$_GET['dwnld']}.php";        header('Content-Disposition: attachment; filename="' . $file . '"');        readfile($file);    }
